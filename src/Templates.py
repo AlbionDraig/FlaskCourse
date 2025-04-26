c2c1e8bde6622c60705b3b71c995ca1610ece5ab
Sebastian Gutierrez Betancourt <sebastiangutierrez.betancourt@gmail.com>
2025-04-26 13:34:31 -0500

    Implement home and repeat routes with dynamic content rendering and add repeat.html template

--- src/Templates.py
+++ src/Templates.py
@@ -8,18 +8,39 @@ from datetime import datetime
 app = Flask(__name__, template_folder='../templates')
 
 @app.add_template_filter
-# app.add_template_filter(format_date, name='format_date')
 def format_date(date, format='%Y-%m-%d'):
     return date.strftime(format)
+# app.add_template_filter(format_date, name='format_date')
 
 @app.route('/')
-def index():
+def home():
     page_name = 'Home'
     datetime_now = datetime.now()
     return render_template(
         'home.html', 
+        page_name=page_name,
+        datetime_now=datetime_now
+    )
+
+# Personalized function
+@app.add_template_global
+def repeat_times(text, times):
+    return text * times
+# @app.add_template_global(repeat_times, name='repeat_times')
+
+@app.route('/repeat')
+@app.route('/repeat/<string:text>/<int:times>')
+def repeat(text = '' , times = 0):
+    page_name = 'Repeat'
+    datetime_now = datetime.now()
+    return render_template(
+        'repeat.html', 
+        page_name=page_name,
         datetime_now=datetime_now,
-        page_name=page_name
+        text=text,
+        times=times,
+        # share functions with templates
+        # repeat_times=repeat_times
     )
 
 @app.route('/<string:name>')

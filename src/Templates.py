7cf62cacf80cb3ade79f1fa998198fba6ce03181
Sebastian Gutierrez Betancourt <sebastiangutierrez.betancourt@gmail.com>
2025-04-26 12:51:38 -0500

    Add home and boiler templates, implement date formatting filter in Flask app

--- src/Templates.py
+++ src/Templates.py
@@ -3,11 +3,26 @@
 # flask --app .\src\Templates.py --debug run
 
 from flask import Flask, render_template
+from datetime import datetime
 
 app = Flask(__name__, template_folder='../templates')
 
+@app.add_template_filter
+# app.add_template_filter(format_date, name='format_date')
+def format_date(date, format='%Y-%m-%d'):
+    return date.strftime(format)
+
 @app.route('/')
+def index():
+    page_name = 'Home'
+    datetime_now = datetime.now()
+    return render_template(
+        'home.html', 
+        datetime_now=datetime_now,
+        page_name=page_name
+    )
+
 @app.route('/<string:name>')
-def index(name=None):
+def greeting(name=None):
     data = ['Hello', 'World', 'Flask', 'Python']
     return render_template('demo.html', name=name, data=data)
